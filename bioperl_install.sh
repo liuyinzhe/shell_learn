cc057554971824b059a66a7d14aeeb7a7b35ccc6
liuyinzhe <54565139+liuyinzhe@users.noreply.github.com>
2022-04-21 14:19:53 +0800

    Create bioperl_install.sh

---
+++ bioperl_install.sh
@@ -0,0 +1,22 @@
+
+<< EOF
+perl -MCPAN -e shell
+cpan>install Module::Build
+cpan>o conf prefer_installer MB
+cpan>o conf commit
+cpan>d /bioperl/
+Distribution    BOZO/Fry-Lib-BioPerl-0.15.tar.gz
+Distribution    CDRAUG/BioPerl-1.7.4.tar.gz
+Distribution    CDRAUG/Dist-Zilla-PluginBundle-BioPerl-0.27.tar.gz
+Distribution    CJFIELDS/BioPerl-1.007002.tar.gz
+Distribution    CJFIELDS/BioPerl-1.6.924.tar.gz
+Distribution    CJFIELDS/BioPerl-1.7.7.tar.gz
+Distribution    CJFIELDS/BioPerl-1.7.8.tar.gz
+Distribution    CJFIELDS/BioPerl-DB-1.006900.tar.gz
+Distribution    CJFIELDS/BioPerl-Network-1.006902.tar.gz
+Distribution    CJFIELDS/BioPerl-Run-1.007002.tar.gz
+Distribution    CJFIELDS/BioPerl-Run-1.007003.tar.gz
+Distribution    CJFIELDS/Bundle-BioPerl-2.1.9.tar.gz
+12 items found
+cpan>install CJFIELDS/BioPerl-1.7.8.tar.gz
+EOF
