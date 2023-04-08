7e7a5cc0e006afb627bcb9689e14734a6f0f2685
liuyinzhe <54565139+liuyinzhe@users.noreply.github.com>
2023-04-08 15:00:11 +0800

    Update find.sh

--- find.sh
+++ find.sh
@@ -3,6 +3,7 @@
 # 类型搜索
 -type d #目录
 -type f #文件
+-type l #link
 
 # 名字搜索
 -name '*.txt'  #通配符
@@ -15,4 +16,10 @@
 -mtime -7 # modfiy 修改时间，7天以内修改的
 -mtime +7 # modfiy 修改时间，7天以外修改的
 
-find /var/log -type f -iname "*.log" -mtime +7 -exec ls -l {} \;
+# 搜索深度控制
+-mindepth
+-maxdepth
+
+find /var/log -type f -mindepth 1 -maxdepth 4 -iname "*.log" -mtime +7 -exec ls -l {} \;
+
+
