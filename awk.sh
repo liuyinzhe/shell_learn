70e7fde199cf4295d44e51c4945e46e2e2ce79ab
liuyinzhe <54565139+liuyinzhe@users.noreply.github.com>
2023-05-10 13:17:50 +0800

    Update awk.sh

--- awk.sh
+++ awk.sh
@@ -1,3 +1,8 @@
 
-#MultiLine to singe line
+# MultiLine to singe line
 awk '{if(/>/){if(length(a)>1){print a;a="";};print $1;}else{a=a""$1;}}END{print a;}' input.fa 
+
+
+# Capitalize the sequence and wrap it with 60 characters
+awk '{if(/^>/){print $_;}else{ a=toupper($0);print gensub(/([ATGCN]{60})/,"\\1\n","g",a);}}'  input.fa
+#https://www.jianshu.com/p/479286cc0360
