d17c554f3438ed1dbe8d029a91c52c6e1231e4bf
liuyinzhe <54565139+liuyinzhe@users.noreply.github.com>
2023-10-02 23:04:32 +0800

    Update find.sh

--- find.sh
+++ find.sh
@@ -2,8 +2,13 @@
 
 # 类型搜索
 -type d #目录
--type f #文件
--type l #link
+-type f #普通文件
+-type l #link 符号连接
+# 不常用
+# c 字符设备
+# b 块设备
+# s 套接字
+# p Fifo
 
 # 名字搜索
 -name '*.txt'  #通配符
@@ -13,8 +18,16 @@
 -size -100M
 
 # 时间搜索
+# UNIX/Linux文件系统每个文件都有三种时间戳：
+# 访问时间 （-atime/天，-amin/分钟）：用户最近一次访问时间。
+# 修改时间 （-mtime/天，-mmin/分钟）：文件最后一次修改时间。
+# 变化时间 （-ctime/天，-cmin/分钟）：文件数据元（例如权限等）最后一次修改时间。
 -mtime -7 # modfiy 修改时间，7天以内修改的
 -mtime +7 # modfiy 修改时间，7天以外修改的
+
+find . -type f -atime -7    #搜索最近七天内被访问过的所有文件
+find . -type f -atime 7     #搜索恰好在七天前被访问过的所有文件
+find . -type f -atime +7    #搜索被访问过超过七天的所有文件
 
 # 搜索深度控制
 -mindepth
@@ -34,15 +47,16 @@ find -type d -empty
 find ./ -name 'qipa250.log' -ok rm -rf {} \;
 
 
-# 排除特定目录(usrA/usrB)
-#find .  -path ./usrA -prune -o  -name "*.stats.txt"
+# 排除特定目录(usrA/usrB)下的文件 
+#find .  -path ./usrA -prune -o  -name "*.stats.txt"  -type f  |grep -E -v "usrA|usrB"
 # 多个目录 # 注释事项 \(\) 两个注释的括号不能与其它字符连接
-find .  \( -path ./usrA -o -path ./usrB \) -prune -o  -name "*.stats.txt" # |xargs -i gzip {} 
+find .  \( -path ./usrA -o -path ./usrB \) -prune -o  -name "*.stats.txt" -type f |grep -E -v "usrA|usrB" |xargs -i gzip {} 
 
   #  -o 是or的 或的作用
   #  -prune  表示忽略
   #  整体思路，先过滤目录，-prune 后再考虑文件名字
-  
+find .  \( -path ./usrA -o -path ./usrB \) -prune  -type d |grep -E -v "usrA|usrB"
+# -prune 仅仅是前缀，需要 额外排除usrA usrB
   #  注意事项：
   #  1)-prune 必须和 -path， -o 一起使用
   #  2）-prune -o 的顺序不 能调换
