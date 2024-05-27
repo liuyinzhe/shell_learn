d9d99375c1a01819b393707b3b004b8faa368320
liuyinzhe <54565139+liuyinzhe@users.noreply.github.com>
2024-05-27 13:02:36 +0800

    Update real_run_time_stat.sh

--- real_run_time_stat.sh
+++ real_run_time_stat.sh
@@ -7,6 +7,30 @@
 # user 0.00
 # sys 0.00
 
+/usr/bin/time -v sleep 2s
+        # Command being timed: "sleep 2s"
+        # User time (seconds): 0.00
+        # System time (seconds): 0.00
+        # Percent of CPU this job got: 0%
+        # Elapsed (wall clock) time (h:mm:ss or m:ss): 0:02.00
+        # Average shared text size (kbytes): 0
+        # Average unshared data size (kbytes): 0
+        # Average stack size (kbytes): 0
+        # Average total size (kbytes): 0
+        # Maximum resident set size (kbytes): 652
+        # Average resident set size (kbytes): 0
+        # Major (requiring I/O) page faults: 0
+        # Minor (reclaiming a frame) page faults: 211
+        # Voluntary context switches: 2
+        # Involuntary context switches: 1
+        # Swaps: 0
+        # File system inputs: 0
+        # File system outputs: 0
+        # Socket messages sent: 0
+        # Socket messages received: 0
+        # Signals delivered: 0
+        # Page size (bytes): 4096
+        # Exit status: 0
 
 /usr/bin/time -f "real %e\nuser %U\nsys %S\nmax_mem %M\nmean_mem %t\nsys_io_i %I\nsys_io_o%Innet_io_i %r\nnet_io_o %s\n\$? %x" sleep 2s
 # real 2.00
